618d000c660fafc890e1d43d1eb08c0a8896f697
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-02-15 16:02:40 +0530

    Added source map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AppConfig, customConfig } from './config';
-
-
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
